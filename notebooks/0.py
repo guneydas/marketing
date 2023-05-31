1b646552e1122e607ce82e276aeeaaf13474de87
Abdullah Guneydas <abdullah.guneydas@esome.com>
2023-05-31 20:53:46 +0200

    make target spend visible

--- notebooks/0.py
+++ notebooks/0.py
@@ -7,89 +7,145 @@ import matplotlib.pylab as plt
 import altair as alt
 
 # %%
-def mmk(spend):
-    vmax = 80000
-    k = 20000
+
+
+# %%
+platform_details = {
+    'Facebook': {
+        'parent': 'Meta',
+        'Vmax': 80000,
+        'k': 20000,
+        'current': 15000,
+    },
+    'Instagram': {
+        'parent': 'Meta',
+        'Vmax': 80000,
+        'k': 30000,
+        'current': 20000,
+    },
+    'Audience Network': {
+        'parent': 'Meta',
+        'Vmax': 40000,
+        'k': 30000,
+        'current': 10000,
+    },
+    'Google Search': {
+        'parent': 'Google',
+        'Vmax': 90000,
+        'k': 5000,
+        'current': 15000,
+    },
+    'Youtube': {
+        'parent': 'Google',
+        'Vmax': 40000,
+        'k': 10000,
+        'current': 15000,
+    },
+    'Google Display Network': {
+        'parent': 'Google',
+        'Vmax': 30000,
+        'k': 20000,
+        'current': 10000,
+    },
+    'Bing Search': {
+        'parent': 'Microsoft',
+        'Vmax': 10000,
+        'k': 2000,
+        'current': 10000,
+    },
+}
+
+# %%
+def mmk(spend, vmax, k):
     return vmax * spend / (spend + k)
 
-# %%
-for s in [10000, 20000, 30000]:
-    print(s, mmk(s))
+def mmk_inverse(revenue, vmax, k):
+    return revenue * k / (vmax - revenue)
+
+def mmk_roas_to_spend(roas, vmax, k):
+    return vmax / roas - k
 
 # %%
-xs = [0]
-last = 0
-while last < 80000:
-    xs += [last + x * (2*i+1) for x in range(1, 100)]
-    last = xs[-1]
-
-
-# %%
-print(len(xs), xs[-1])
-
-# %%
-plt.plot(xs, [mmk(x) for x in xs])
-plt.plot(xs, [270*np.sqrt(x) for x in xs])
-plt.plot(xs, [2000*np.power(x, 0.3) for x in xs])
-
-# %%
-
-
-# %%
-def mmk(spend):
-    vmax = 80000
-    k = 20000
+def mmk(spend, vmax, k):
     return vmax * spend / (spend + k)
 
-df = pd.DataFrame({'spend': [100*i for i in range(1, 801)]})
-df['revenue'] = df.spend.apply(mmk).astype('int')
+def mmk_inverse(revenue, vmax, k):
+    return revenue * k / (vmax - revenue)
+
+def mmk_roas_to_spend(roas, vmax, k):
+    return vmax / roas - k
+
+rows = []
+target_roas_rows = []
+current_rows = []
+for platform, details in platform_details.items():
+    # calculate values for every 100s of spend and
+    for i in range(1, max(details['Vmax'], details['current']) // 100 + 1):
+        spend = 100 * i
+        revenue = round(mmk(spend, details['Vmax'], details["k"]))
+        rows.append({
+            'Spend': spend,
+            'Revenue': revenue,
+            'ROAS': revenue / spend,
+            'Platform': platform,
+            'Parent': details['parent'],
+        })
+    # calculate values for every 0.01s of roas
+    min_roas = round(revenue / spend, 2)  # last calculated roas (for highest spend)
+    max_roas = round(mmk(100, details['Vmax'], details["k"]) / 100, 2)  # for min spend
+    for roas in np.arange(min_roas, max_roas + 0.01, 0.01):
+        spend = mmk_roas_to_spend(roas, details['Vmax'], details["k"])
+        revenue = mmk(spend, details['Vmax'], details["k"])
+        row = {
+            'Spend': round(spend),
+            'Revenue': round(revenue),
+            'ROAS': round(100 * roas),
+            'Platform': platform,
+            'Parent': details['parent'],
+        }
+        target_roas_rows.append(row)
+        #if spend % 100 != 0:
+        #    rows.append(row)
+    # calculate values for current spend
+    spend = round(details['current'])
+    revenue = round(mmk(spend, details['Vmax'], details["k"]))
+    row = {
+        'Spend': spend,
+        'Revenue': revenue,
+        'ROAS': revenue / spend,
+        'Platform': platform,
+        'Parent': details['parent'],
+    }
+    if spend % 100 != 0:
+        rows.append(row)
+    current_rows.append(row)
+
+df_base = pd.DataFrame(rows).sort_values(['Parent', 'Platform', 'Spend']).reset_index(drop=True)
+del rows
+
+df_target_roas = pd.DataFrame(target_roas_rows).sort_values(['Parent', 'Platform', 'Spend']).reset_index(drop=True)
+del target_roas_rows
+
+df_current = pd.DataFrame(current_rows).sort_values(['Parent', 'Platform', 'Spend']).reset_index(drop=True)
+del current_rows
 
 # %%
-df
+df_current
 
 # %%
-alt.Chart(df).mark_line().encode(x='spend', y='revenue').interactive()
+df_base
 
 # %%
-alt.Chart(df).mark_line().encode(
-    alt.X('spend', title='Spend'),
-    alt.Y('revenue', title='Revenue'),
-)
+df_base[df_base.Platform == 'Facebook']
 
 # %%
-def mmk(spend):
-    vmax = 80000
-    k = 20000
-    return vmax * spend / (spend + k)
-
-df = pd.DataFrame({'Spend': [100*i for i in range(1, 801)]})
-df['Revenue'] = df.Spend.apply(mmk).astype('int')
+df_target_roas[df_target_roas.Platform == 'Facebook']
 
 # %%
-alt.Chart(df).mark_line().encode(
-    alt.X('Spend'),
-    alt.Y('Revenue'),
-    tooltip = [
-        alt.Tooltip('Spend'),
-        alt.Tooltip('Revenue'),
-    ],
-)
+df_base.isnull().sum()
 
 # %%
-df['ROAS'] = df.Revenue / df.Spend
-df.ROAS.hist(bins=100)
-
-# %%
-df.head()
-
-# %%
-df.tail()
-
-# %%
-df[df.Spend >= 19900]
-
-# %%
-np.arange(0.8, 4, 0.1)
+np.arange(0.8, 3.91, 0.01)
 
 # %%
 vmax = 80000
@@ -104,30 +160,17 @@ def mmk_inverse(revenue):
 def mmk_roas_to_spend(roas):
     return vmax / roas - k
 
-# %%
-mmk(20000), mmk_inverse(mmk(20000))
-
-# %%
-mmk(100000), mmk_inverse(mmk(100000))
-
-# %%
-mmk_roas_to_spend(3)
-
-# %%
-df[df.Spend == 6700]
-
-# %%
 df = pd.DataFrame({'Spend': [100*i for i in range(1, 801)]})
 df['Revenue'] = df.Spend.apply(mmk).astype('int')
 df['ROAS'] = df.Revenue / df.Spend
 
 new_rows = []
-for roas in np.arange(0.8, 4, 0.1):
+for roas in np.arange(0.8, 3.91, 0.01):
     spend =  mmk_roas_to_spend(roas)
     revenue = mmk(spend)
     if (spend not in df.Spend.values) and (roas not in df.ROAS.values):
         new_rows.append({
-            'ROAS': round(roas, 1),
+            'ROAS': round(roas, 2),
             'Spend': round(spend),
             "Revenue": round(revenue),
         })
@@ -135,57 +178,22 @@ for roas in np.arange(0.8, 4, 0.1):
 df = pd.concat([df, pd.DataFrame(new_rows)], ignore_index=True).sort_values('Spend').reset_index(drop=True)
 
 # %%
-df.head(15)
+df
 
 # %%
-df.tail()
+df[df.Spend % 100 != 0]
 
 # %%
-2000/513
+df_base[(df_base.Platform == 'Facebook') & df_base.Spend % 100 != 0]
 
 # %%
-new_rows
+len(np.arange(0.8, 3.91, 0.01))
 
 # %%
-
-
-# %%
-
+df_base[(df_base.Platform == 'Facebook') & (df_base.ROAS.isin(np.arange(0.8, 3.91, 0.01)))]
 
 # %%
-slider = alt.binding_range(min=0.8, max=3.9, step=-0.1, name='ROAS:')
-roas_var = alt.Parameter('roas', value=2.0, bind=slider)
-
-highlight = alt.Chart(df[df.ROAS == roas_var]).mark_point(color='orange', size=10).encode(
-    alt.X('Spend'),
-    alt.Y('Revenue'),
-)
-
-highlight
-
-# %%
-alt.__version__
-
-# %%
-
-
-# %%
-slider = alt.binding_range(min=0.8, max=3.9, step=-0.1, name='ROAS:')
-highlight = alt.Chart(df[df.ROAS == slider]).mark_circle(color='orange').encode(
-    alt.X('Spend'),
-    alt.Y('Revenue'),
-)
-
-main_chart = alt.Chart(df).mark_line().add_selection(highlight).encode(
-    alt.X('Spend', title="Spend (weekly)"),
-    alt.Y('Revenue'),
-    tooltip = [
-        alt.Tooltip('Spend'),
-        alt.Tooltip('Revenue'),
-    ],
-)
-
-main_chart
+df[df.ROAS.isin(np.arange(0.8, 3.91, 0.01))]
 
 # %%
 
--- source/streamlit_app.py
+++ source/streamlit_app.py
@@ -4,21 +4,10 @@ import altair as alt
 import streamlit as st
 
 
-
-def mmk(spend, vmax, k):
-    return vmax * spend / (spend + k)
-
-def mmk_inverse(revenue, vmax, k):
-    return revenue * k / (vmax - revenue)
-
-def mmk_roas_to_spend(roas, vmax, k):
-    return vmax / roas - k
-
-
 platform_details = {
     'Facebook': {
         'parent': 'Meta',
-        'Vmax': 60000,
+        'Vmax': 80000,
         'k': 20000,
         'current': 15000,
     },
@@ -61,11 +50,22 @@ platform_details = {
 }
 
 
+def mmk(spend, vmax, k):
+    return vmax * spend / (spend + k)
+
+def mmk_inverse(revenue, vmax, k):
+    return revenue * k / (vmax - revenue)
+
+def mmk_roas_to_spend(roas, vmax, k):
+    return vmax / roas - k
+
+
 rows = []
+target_roas_rows = []
 current_rows = []
 for platform, details in platform_details.items():
     # calculate values for every 100s of spend and
-    for i in range(1, 801):
+    for i in range(1, max(details['Vmax'], details['current']) // 100 + 1):
         spend = 100 * i
         revenue = round(mmk(spend, details['Vmax'], details["k"]))
         rows.append({
@@ -79,16 +79,18 @@ for platform, details in platform_details.items():
     min_roas = round(revenue / spend, 2)  # last calculated roas (for highest spend)
     max_roas = round(mmk(100, details['Vmax'], details["k"]) / 100, 2)  # for min spend
     for roas in np.arange(min_roas, max_roas + 0.01, 0.01):
-        spend = round(mmk_roas_to_spend(roas, details['Vmax'], details["k"]))
-        revenue = round(mmk(spend, details['Vmax'], details["k"]))
-        if spend % 100 != 0:
-            rows.append({
-                'Spend': spend,
-                'Revenue': revenue,
-                'ROAS': roas,
-                'Platform': platform,
-                'Parent': details['parent'],
-            })
+        spend = mmk_roas_to_spend(roas, details['Vmax'], details["k"])
+        revenue = mmk(spend, details['Vmax'], details["k"])
+        row = {
+            'Spend': round(spend),
+            'Revenue': round(revenue),
+            'ROAS': round(100 * roas),
+            'Platform': platform,
+            'Parent': details['parent'],
+        }
+        target_roas_rows.append(row)
+        #if spend % 100 != 0:
+        #    rows.append(row)
     # calculate values for current spend
     spend = round(details['current'])
     revenue = round(mmk(spend, details['Vmax'], details["k"]))
@@ -99,13 +101,17 @@ for platform, details in platform_details.items():
         'Platform': platform,
         'Parent': details['parent'],
     }
-    rows.append(row)
+    if spend % 100 != 0:
+        rows.append(row)
     current_rows.append(row)
 
-df = pd.DataFrame(rows).sort_values(['Parent', 'Platform', 'Spend'])
+df_base = pd.DataFrame(rows).sort_values(['Parent', 'Platform', 'Spend']).reset_index(drop=True)
 del rows
 
-df_current = pd.DataFrame(current_rows).sort_values(['Parent', 'Platform', 'Spend'])
+df_target_roas = pd.DataFrame(target_roas_rows).sort_values(['Parent', 'Platform', 'Spend']).reset_index(drop=True)
+del target_roas_rows
+
+df_current = pd.DataFrame(current_rows).sort_values(['Parent', 'Platform', 'Spend']).reset_index(drop=True)
 del current_rows
 
  
@@ -117,17 +123,22 @@ st.set_page_config(
 
 
 st.title("Ad Spend - Revenue relation")
-
 st.write("See the relation between your Ad Spend and the Revenue it generates for each platform.")
+
+
+df = df_base
 
 col1, col2, col3 = st.columns(3)
 with col1:
+    st.text("\u25CB: Current Spend")
+    st.text("\u25A1: Target Spend")
+with col2:
     target_roas = st.slider(
-            "Choose target ROAS", min_value=1., max_value=3.9, step=0.01, value=2.0
-        )
+        "Choose target ROAS", min_value=1., max_value=4., step=0.01, value=2.0
+    )
 
-
-line = alt.Chart(df).mark_line().encode(
+line = alt.Chart(df).mark_line(
+).encode(
     alt.X('Spend', title="Spend (weekly)"),
     alt.Y('Revenue'),
     color = 'Platform',
@@ -137,8 +148,9 @@ line = alt.Chart(df).mark_line().encode(
         alt.Tooltip('Revenue'),
     ],
 )
-
-target_point = alt.Chart(df[df.ROAS == target_roas]).mark_square(
+target_point = alt.Chart(
+    df_target_roas[df_target_roas.ROAS == round(100*target_roas)]
+).mark_square(
     size=100,
     opacity=0.8,
 ).encode(
@@ -146,9 +158,9 @@ target_point = alt.Chart(df[df.ROAS == target_roas]).mark_square(
     alt.Y('Revenue'),
     color = 'Platform',
 )
-
-
-current_point = alt.Chart(df_current).mark_circle(
+current_point = alt.Chart(
+    df_current
+).mark_circle(
     size=100,
     opacity=0.5,
 ).encode(
@@ -156,24 +168,18 @@ current_point = alt.Chart(df_current).mark_circle(
     alt.Y('Revenue'),
     color = 'Platform',
 )
-
-
-
-
-
-
-
 st.altair_chart((
     line + target_point  + current_point
-), use_container_width=True, )
+), use_container_width=True)
+
+
 
 
 # https://discuss.streamlit.io/t/remove-made-with-streamlit-from-bottom-of-app/1370/16
 hide_streamlit_style = """
             <style>
-            #MainMenu {visibility: hidden;}
+            MainMenu {visibility: hidden;}
             footer {visibility: hidden;}
             </style>
             """
 st.markdown(hide_streamlit_style, unsafe_allow_html=True) 
-
