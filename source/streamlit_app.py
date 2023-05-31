4a5adab9ebc10af46fe1fc5540d3f1bf18564802
Abdullah Guneydas <abdullah.guneydas@esome.com>
2023-05-31 22:07:16 +0200

    add another tab

--- source/streamlit_app.py
+++ source/streamlit_app.py
@@ -129,13 +129,13 @@ st.write("See the relation between your Ad Spend and the Revenue it generates fo
 
 
 
-tab1, tab2, tab3, tab4 = st.tabs(["1", "2", "3", "4"])
+tab1, tab2, tab3, tab4, tab5 = st.tabs(["1", "2", "3", "4", "5"])
 
 with tab1:
     df = df_base[df_base.Platform == 'Facebook']
 
     target_roas = st.slider(
-        "Choose target ROAS", min_value=0.8, max_value=3.9, step=0.01, value=2.0
+        "Choose target ROAS", min_value=0.8, max_value=3.9, step=0.01, value=2.0,
     )
 
     line = alt.Chart(df).mark_line(
@@ -202,7 +202,7 @@ with tab2:
         st.text("\u25A1: Target Spend")
     with col2:
         target_roas = st.slider(
-            "Choose target ROAS", min_value=1., max_value=4., step=0.01, value=2.0
+            "Choose target ROAS", min_value=1., max_value=4., step=0.01, value=2.0,
         )
     
     line = alt.Chart(df).mark_line(
@@ -246,12 +246,10 @@ with tab2:
 with tab3:
     df = df_base
 
-    col1, col2 = st.columns(2)
+    col1, col2 = st.columns(2, gap='large')
     with col1:
         platform = st.selectbox("Choose a Platform:", df.Platform.unique())
-    st.text("")
-    col1, col2 = st.columns(2)
-    with col1:
+    with col2:
         max_roas = df.loc[df.Platform == platform, "ROAS"].max()
         target_roas = st.slider(
             "Choose target ROAS",
@@ -316,6 +314,77 @@ with tab3:
 
 with tab4:
     df = df_base
+
+    col1, col2 = st.columns(2)
+    with col1:
+        platform = st.selectbox("Choose a Platform:", df.Platform.unique(), key=1)
+    st.text("")
+    col1, col2 = st.columns(2)
+    with col1:
+        max_roas = df.loc[df.Platform == platform, "ROAS"].max()
+        target_roas = st.slider(
+            "Choose target ROAS",
+            min_value=1., max_value=round(max_roas, 1), step=0.01, value=2.0, key=2,
+        )
+
+    line = alt.Chart(
+        df[df.Platform == platform]
+        ).mark_line().encode(
+            alt.X('Spend', title="Spend (weekly)"),
+            alt.Y('Revenue'),
+        tooltip = [
+            alt.Tooltip('Spend'),
+            alt.Tooltip('Revenue'),
+        ],
+    )
+    target_point = alt.Chart(
+        df_target_roas[(df_target_roas.Platform == platform) & (df_target_roas.ROAS == round(100*target_roas))]
+    ).mark_point(
+        color='orange',
+        size=100,
+        opacity=0.8,
+    ).encode(
+        alt.X('Spend'),
+        alt.Y('Revenue'),
+    )
+    current_point = alt.Chart(
+        df_current[df_current.Platform == platform]
+    ).mark_point(
+        color='red',
+        size=100,
+        opacity=0.5,
+    ).encode(
+        alt.X('Spend'),
+        alt.Y('Revenue'),
+    )
+    annotation_layer = (
+        alt.Chart(
+            df_target_roas[(df_target_roas.Platform == platform) & (df_target_roas.ROAS == round(100*target_roas))]
+        ).mark_text(
+            size=12, text='Target', color='orange', dy=-10, dx=-15, opacity=0.8
+        ).encode(
+            x='Spend',
+            y='Revenue'
+        ) +
+        alt.Chart(
+            df_current[df_current.Platform == platform]
+        ).mark_text(
+            size=12, text='Current', color='red', dy=10, dx=15, opacity=0.5
+        ).encode(
+            x='Spend',
+            y='Revenue'
+        )
+    )
+
+    st.altair_chart((
+        line + target_point  + current_point + annotation_layer
+    ), use_container_width=True)
+
+
+
+
+with tab5:
+    df = df_base
     
     line = alt.Chart(df).mark_line(
     ).encode(
@@ -329,6 +398,7 @@ with tab4:
     ).facet(row="Platform")
 
     st.altair_chart(line, use_container_width=True)
+
 
 
 
